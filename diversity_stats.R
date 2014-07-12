7e567d8434bfe2ca3d7a5a3cc18d8192a03fe83b
ebach <ebach@iastate.edu>
2014-07-12 18:17:12 -0500

    removed spaces from taxonomy csv, corrected code in diversity_stats

--- diversity_stats.R
+++ diversity_stats.R
@@ -5,6 +5,7 @@ head(data.nosing.rar[,1:10])
 #calculating richness, shannons, and evenness
 
 richness<-fisher.alpha(data.nosing.rar[,-c(1:5)],1)
+head(richness)
 shannons<-diversity(data.nosing.rar[,-c(1:5)])
 evenness<-shannons/log(richness)
 hist(richness)
@@ -13,12 +14,28 @@ div_stats<-data.frame(data.nosing.rar[,1:5],richness,shannons,evenness)
 
 head(div_stats)
 
-#looking at shannons first
-#EB:  Come back here as do this for other diversity measures
+#looking at data distribution
 ggplot(div_stats)+geom_histogram(aes(shannons^2.7))
+ggplot(div_stats)+geom_histogram(aes(richness^2.7))
+ggplot(div_stats)+geom_histogram(aes(evenness^2.7))
 
+#richness is skewed heavily to right
+ggplot(div_stats)+geom_histogram(aes(richness))
+#keeping log transformation helps normalize data
+
+#Testing main effects of date, crop, soil frac on diversity measures
 summary(test<-aov(richness~Date+Crop+SoilFrac, data=div_stats))
 TukeyHSD(test)
+#SoilFrac P=0.001, micro > LM, WS
+
+summary(test<-aov(evenness~Date+Crop+SoilFrac, data=div_stats))
+TukeyHSD(test)
+#no effects of any main factor
+
+summary(test<-aov(shannons~Date+Crop+SoilFrac, data=div_stats))
+TukeyHSD(test)
+#SoilFrac P=0.02, micro>LM
+
 ?diversity
 
 ggplot(div_stats)+geom_boxplot(aes(x=SoilFrac, y=richness))
@@ -52,15 +69,15 @@ return(quantile(boot.mean,(0.025)))
 }
 
 library(plyr)
-stats<-ddply(data_taxa2, .(SoilFrac, Phylum), summarise,.progress="text",
+Phyla.data<-ddply(data_taxa2, .(SoilFrac, Phylum), summarise,.progress="text",
 mean=mean(value),
 high95=boot.high(value),
 low95=boot.low(value)
 )
-head(stats)
+head(Phyla.data)
 
 ?geom_pointrange
-ggplot(stats)+geom_pointrange(aes(x=Phylum,y=mean,ymax=high95,ymin=low95, color=SoilFrac),position=position_dodge(width=1))+coord_flip()+scale_y_log10()
+ggplot(Phyla.data)+geom_pointrange(aes(x=Phylum,y=mean,ymax=high95,ymin=low95, color=SoilFrac),position=position_dodge(width=1))+coord_flip()+scale_y_log10()
 stats$SoilFrac<-factor(stats$SoilFrac, levels=c("Micro","SM","MM","LM","WS"))
-ggplot(stats)+geom_pointrange(aes(x=order,y=mean,ymax=high95,ymin=low95,colour=SoilFrac),position=position_dodge(width=.5))+coord_flip()+scale_y_log10()
+ggplot(stats)+geom_pointrange(aes(x=Phylum,y=mean,ymax=high95,ymin=low95,colour=SoilFrac),position=position_dodge(width=.5))+coord_flip()+scale_y_log10()
 
