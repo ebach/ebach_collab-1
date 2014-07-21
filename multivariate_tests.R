506e0a1a5f93425b04a389672a71fc45515500a4
ebach <ebach@iastate.edu>
2014-07-21 17:04:19 -0500

    minor edits to code

--- multivariate_tests.R
+++ multivariate_tests.R
@@ -82,11 +82,14 @@ X1
 mds<-metaMDS(decostand(subset(data.nosing.reads, reads>100)[,-c(1:6)],"pa" ),k=6,autotransform=FALSE)
 ggplot.NMDS(mds, (subset(data.nosing.reads, reads>100)$Crop), rainbow(3))
 fig_16S<-ggplot.NMDS(mds, (data.nosing.rar$Crop), rainbow(3))
-#EBach, I get an error on the "fig_16S" saying MDS1, MDS2, and Treatment have differing number of rows (114 vs. 100)
-#also, should be fig_ITS?
+#EBach, is fig_16S just the "source" code for the ggplot.NMDS figure?
+
+#Date ("abundance")
+ggplot.NMDS(mds, (subset(data.nosing.reads, reads>100)$Date), rainbow(2))
 
 # doing the mds for soilfrac based on p/a 
 mds<-metaMDS(decostand(data.nosing.rar[,-c(1:5)],"pa"),k=2,autotransform=FALSE)
 ggplot.NMDS(mds,data.nosing.rar$SoilFrac, rainbow(5))
-
+head(mds)
+head(data.nosing.reads)
 
